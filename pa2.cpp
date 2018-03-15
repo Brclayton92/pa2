47ebf3ef3ce8fa4f5590c4c576fe23ee1b365aaf
Brock <Brock.clayton92@gmail.com>
2018-03-15 15:54:28 -0400

    0.4:
    -debugged best fit method

--- pa2.cpp
+++ pa2.cpp
@@ -47,37 +47,68 @@ void LinkedList::printList(){
 }
 
 void LinkedList::bestFit(string progName, int sizeReq) {
-    sizeReq = sizeReq / 4;
-    node *start = nullptr;
-    node *temp = head->next;
-    node *posStart = head->next;
+    if (sizeReq % 4 == 0) {
+        sizeReq = sizeReq / 4;
+    }
+    else {
+        sizeReq = (sizeReq / 4) + 1;
+    }
+    node *start = head;
+    node *temp = head;
+    node *posStart = head;
     int smallestFit = size;
     int length = 0;
+    bool wasInside = false;
 
+    //iterates through linked list
     for (int i = 0; i < size; i++){
-        if (temp->data == "free" && length ==0){
-            length++;
-            posStart = temp;
+
+        // check for free space where temp is currently pointing
+        if (temp->data == "Free"){
+            //if current node pointed to by temp is free, stores that nodes locations as a possible start, and increments length
+            if (length == 0) {
+                posStart = temp;
+                length++;
+            }
+
+            //if node pointed to by temp is free and length != 0, increments length to count number of free spaces in a row
+            else {
+                length++;
+            }
         }
 
-        else if (temp->data == "free"){
-            length++;
-        }
+        //if temp no longer points to a free node, checks if program will fit in current chunk, if it will, sets smallest fit to the length of current chunk
+        if (temp->data != "Free") {
+            if ((length >= sizeReq)){
 
-        else {
-            if (length >= sizeReq){
                 if (length < smallestFit){
+                    smallestFit = length;
                     start = posStart;
                 }
             }
 
+            //resets length to 0 for use in next free chunk
             length = 0;
         }
 
         temp = temp->next;
     }
 
-    for (int i = 0; i < sizeReq; i++){
+    //checks to ensure the final chunk of space in the linked list isnt the best fit, if it is best fit, start is set to posStart
+    if(start != posStart) {
+        temp = posStart;
+        int counter = 0;
+        while (temp) {
+            counter++;
+            temp = temp->next;
+        }
+        if(counter >= sizeReq && counter < smallestFit) {
+            start = posStart;
+        }
+    }
+
+    //assigns the program to it's appropriate place in memory
+    for (int i = 0; i < sizeReq; i++) {
         start->data = progName;
         start = start->next;
     }
@@ -89,7 +120,15 @@ void LinkedList::bestFit(string progName, int sizeReq) {
 
 int main() {
     LinkedList list;
+    list.printList();
+    cout << endl << endl;
     list.bestFit("P1", 13);
+    list.printList();
+    cout << endl << endl;
+    list.bestFit("P2", 9);
+    list.printList();
+    cout << endl << endl;
+    list.bestFit("P3", 18);
     list.printList();
 
     return 0;
